c28a97c5c0ff8f0b3622472ecb8bc0b03e86bdee
tjw <114938661+tjw2003@users.noreply.github.com>
2023-09-26 21:19:52 +0800

    url还没整明白，今天摸了，明天一定整完

--- src/routes/(main)/studentMain/+page.svelte
+++ src/routes/(main)/studentMain/+page.svelte
@@ -31,6 +31,21 @@ let learningcourses: any = [];
       getLearningCourse();
     });
 
+
+    // function createCourse() {
+    //   postByJson("api/course/create", {
+    //     ID,
+    //     coursename,
+    //     TeacherID,
+    //   })
+    //     .then((res) => {
+    //       console.log("[then]: ", res);
+    //       alert("你好，成功选课");
+    //     })
+    //     .catch((err) => {
+    //       console.log("[error]: ", err);
+    //     });
+    // }
     import { Button } from "flowbite-svelte";
 </script>
 
@@ -44,18 +59,18 @@ let learningcourses: any = [];
     >创建课程</Button>
     <div>教的课程</div>
     {#each teachingCourses as teachingCourse}
-      <a href="/" class="text-black underline">{teachingCourse.name}</a>
+      <a href="/course?courseId={teachingCourse.ID}" class="text-black underline">{teachingCourse.name}</a>
     {/each}
-    <a href="/" class="text-black underline">我**,你**</a>
-    <a href="/" class="text-black underline">我*你**的</a>
+    <a href="/course" class="text-black underline">我**,你**</a>
+    <a href="/course" class="text-black underline">我*你**的</a>
 
     <div class="border-t border-gray-400 my-4"></div>
 
     <div>学的课程</div>
     {#each learningcourses as learningcourse}
-      <a href="/" class="text-black underline">{learningcourse.name}</a>
+      <a href="/course?courseId={learningcourse.ID}" class="text-black underline">{learningcourse.name}</a>
     {/each}
-    <a href="/" class="text-black underline">Home</a>
+    <a href="/course" class="text-black underline">Home</a>
   </div>
 
   <div class="flex-1 p-4">我*,原</div>
