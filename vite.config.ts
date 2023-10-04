bdbfba0f3a2d834e8874384cafc9ba2c7cfff913
Azur冰弦 <973562770@qq.com>
2023-10-04 16:13:00 +0800

    new solid project with unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
-import solid from "solid-start/vite";
-import staticAdapter from "solid-start-static";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+// import devtools from 'solid-devtools/vite';
+import UnocssPlugin from '@unocss/vite';
 
 export default defineConfig({
-  plugins: [solid({
-    adapter: staticAdapter()
-  })],
+  plugins: [
+    /* 
+    Uncomment the following line to enable solid-devtools.
+    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
+    */
+    // devtools(),
+    solidPlugin(),
+    UnocssPlugin({
+      // your config or in uno.config.ts
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext',
+  },
 });
