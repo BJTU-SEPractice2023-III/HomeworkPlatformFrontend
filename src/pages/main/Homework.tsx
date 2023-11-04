92458b4a9df9fcbace96f35d3582e64695dd4695
tjw <114938661+tjw2003@users.noreply.github.com>
2023-11-04 16:37:27 +0800

    ...

--- src/pages/main/Homework.tsx
+++ src/pages/main/Homework.tsx
@@ -1,12 +1,16 @@
-import { useParams} from '@solidjs/router';
-
+import { useParams } from '@solidjs/router';
+import { For, Match, Show, Switch, createSignal, onMount } from 'solid-js';
+import { homeworklists } from '../../lib/homework'
 export default function Homework() {
-    const params = useParams();
+  const params = useParams();
 
+  onMount(() => {
+    
+  })
 
   return (
     <div>
-        你好
+      你好
     </div>
   );
 }
